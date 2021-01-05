ba2f5d6a6b27ec51b283f7c9b5427bfb4ca66c92
root <root@RDL724707.raiadrogasil.com>
2021-01-05 17:35:10 -0300

    update exemplos aula5 mapas e threading

--- idea-projects/estoque/src/estoque/aula5.clj
+++ idea-projects/estoque/src/estoque/aula5.clj
@@ -32,10 +32,59 @@
 (println estoque)
 (println (update estoque :mochila tira-um))
 (println estoque)
-(println (update estoque :mochila #(- % 3)))                 ; chamando update mapa com lambda
+(println (update estoque :mochila #(- % 3)))                ; chamando update mapa com lambda
 (println estoque)
 
 ; retirar do mapa
 (println "Usando dissoc em mapas")
 (println estoque)
 (println (dissoc estoque :mochila))
+
+;;;
+; mapas aninhados
+;;;
+
+(println "\n\n\n")
+
+(def pedido {:mochila  {:quantidade 2, :preco 80}
+             :camiseta {:quantidade 3, :preco 40}})
+(println pedido)
+
+(def pedido (assoc pedido :chaveiro {:quantidade 1, :preco 10}))
+(println pedido)
+(println (pedido :mochila))                                 ; usando mapa como funcao, se pedido for null, vai lancar NullPointerException
+(println (get pedido :mochila))
+(println (get pedido :cadeira))
+(println (get pedido :cadeira {}))                          ; get com valor default {}
+(println (:mochila pedido))
+(println (:cadeira pedido {}))
+
+; modo prefix de chamadas de funcao
+(println (:quantidade (:mochila pedido)))
+
+(println pedido)
+(println (update-in pedido [:mochila :quantidade] inc))
+
+; THREADING FIRST
+(println (-> pedido
+             :mochila
+             :quantidade))
+
+; ou...
+(-> pedido :mochila :quantidade println)
+
+;;;
+; exercicios
+;;;
+
+(def clientes {
+               :15 {
+                    :nome "Ivo"
+                    :certificados ["Clojure" "Java" "Machine Learning"]
+                    }
+               })
+
+(-> clientes :15 :certificados count)
+
+
+
