c5e18ce582719ee9fc638f6fa6f3862152b50c08
root <root@RDL724707.raiadrogasil.com>
2021-01-11 19:18:50 -0300

    add exemplo aula 6

---
+++ idea-projects/estoque/src/estoque/aula6.clj
@@ -0,0 +1,70 @@
+(ns estoque.aula6)
+
+
+(def pedido {:mochila  {:quantidade 2, :preco 80}
+             :camiseta {:quantidade 3, :preco 40}})
+
+(defn imprime-e-15 [valor]
+  (println "valor" (class valor) valor)
+  15)
+
+(println (map imprime-e-15 pedido))
+
+;(defn imprime-e-15 [chave valor]
+;  (println chave "<e>" valor)
+;  15)
+
+(println (map imprime-e-15 pedido))
+
+(defn imprime-e-15 [[chave valor]]
+  (println chave "<e>" valor)
+  15)
+
+(println (map imprime-e-15 pedido))
+
+
+(defn preco-dos-produtos [[chave valor]]
+  (* (:quantidade valor) (:preco valor)))
+
+(println (map preco-dos-produtos pedido))
+(println (reduce + (map preco-dos-produtos pedido)))
+
+(defn preco-dos-produtos [[_ valor]]
+  (* (:quantidade valor) (:preco valor)))
+
+(println (map preco-dos-produtos pedido))
+(println (reduce + (map preco-dos-produtos pedido)))
+
+(defn total-do-pedido
+  [pedido]
+  (reduce + (map preco-dos-produtos pedido)))
+
+(println (total-do-pedido pedido))
+
+;;; THREAD FIRST (simbolo ->)
+;;; THREAD LAST  (simbolo ->>)
+
+; funcao total-do-pedido com thread last
+(defn total-do-pedido
+  [pedido]
+  (->> pedido
+       (map preco-dos-produtos ,,,)
+       (reduce + ,,,)))
+
+(println (total-do-pedido pedido))
+
+
+(defn preco-total-do-produto
+  [produto]
+  (* (:quantidade produto) (:preco produto)))
+
+(defn total-do-pedido
+  [pedido]
+  (->> pedido
+       vals
+       (map preco-total-do-produto)
+       (reduce +)))
+
+(println (total-do-pedido pedido))
+
+
