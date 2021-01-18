bcb4f91f4a6f1abb0f7f45fe5a16c240c85027c7
root <root@RDL724707.raiadrogasil.com>
2021-01-18 14:53:13 -0300

    add exemplo aula 6

--- idea-projects/estoque/src/estoque/aula6.clj
+++ idea-projects/estoque/src/estoque/aula6.clj
@@ -68,3 +68,59 @@
 (println (total-do-pedido pedido))
 
 
+
+(def pedido {:mochila  {:quantidade 2, :preco 80}
+             :camiseta {:quantidade 3, :preco 40}
+             :chaveiro {:quantidade 1}})
+
+(defn gratuito?
+  [item]
+  (<= (get item :preco 0) 0))
+
+(println (filter gratuito? (vals pedido)))
+
+; versao de gratuito? com destruct
+(defn gratuito?
+  [[_ item]]                                                ; entre colchetes o map "destructed"
+  (<= (get item :preco 0) 0))
+
+(println (filter gratuito? pedido))
+
+; versao com lambda
+(defn gratuito?
+  [item]
+  (<= (get item :preco 0) 0))
+
+(println (filter (fn [[chave item]] (gratuito? item)) pedido))
+(println (filter #(gratuito? (second %)) pedido))
+
+(defn pago?
+  [item]
+  (not (gratuito? item)))
+
+(println (pago? {:preco 50}))
+(println (pago? {:preco 0}))
+
+; composicao de funcoes
+(println ((comp not gratuito?) {:preco 50}))
+
+(def pago? (comp not gratuito?))                            ; comp retorna uma funcao
+(println (pago? {:preco 50}))
+(println (pago? {:preco 0}))
+
+
+;;;
+; exercicio de map reduce
+;;;
+
+(def clientes [{:nome "Guilherme" :certificados ["Clojure" "Java" "ML"]}
+               {:nome "Paulo" :certificados ["Java" "CS"]}
+               {:nome "Daniela" :certificados ["Arquitetura" "Gastronomia"]}])
+
+(println clientes)
+(println (map :certificados clientes))
+(println (map count (map :certificados clientes)))
+(println (reduce + (map count (map :certificados clientes))))
+
+(println (->> clientes (map :certificados) (map count) (reduce +)))
+
